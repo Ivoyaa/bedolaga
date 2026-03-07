02663b11a010fa427345217a95ca6125ded63bd7
Ivoyaa <roma57995@gmail.com>
2026-03-07 20:35:03 +0300

    just stuff

--- bedolaga-mvp/src/scala/bedolaga/Build.scala
+++ bedolaga-mvp/src/scala/bedolaga/Build.scala
@@ -2,13 +2,22 @@ package bedolaga
 
 import com.typesafe.config.ConfigFactory
 import fansi._
+import scala.tools.nsc._
 
 import java.io.File
 
 object Build extends App {
-
-
-  println(
-    ProjectStructure.parse("build-structure")(ConfigFactory.parseFile(new File("example/setup.conf")))
+  val project = ProjectStructure.parse("build-structure")(
+    ConfigFactory.parseFile(new File("example/setup.conf"))
   )
+
+  val settings = new Settings()
+  settings.usejavacp.value = true
+  settings.outputDirs.setSingleOutput(project.directory)
+  val global = new Global(settings)
+
+  val run = new global.Run
+
+  run.compile(List(project.fileToCompile))
+
 }
--- bedolaga-mvp/src/scala/bedolaga/ProjectStructure.scala
+++ bedolaga-mvp/src/scala/bedolaga/ProjectStructure.scala
@@ -1,13 +1,18 @@
 package bedolaga
 
-import com.typesafe.config.{ConfigFactory, Config}
+import com.typesafe.config.{Config, ConfigFactory}
 
-final case class ProjectStructure(name: String, mainClass: String)
+final case class ProjectStructure(name: String, mainClass: String, directory: String, fileToCompile: String)
 
 object ProjectStructure {
   def parse(path: String)(config: Config): ProjectStructure = {
     val conf = config.getConfig(path)
 
-    ProjectStructure(name = conf.getString("name"), mainClass = conf.getString("main-class"))
+    ProjectStructure(
+      name = conf.getString("name"),
+      mainClass = conf.getString("main-class"),
+      directory = conf.getString("directory"),
+      fileToCompile = conf.getString("file-to-compile")
+    )
   }
 }
